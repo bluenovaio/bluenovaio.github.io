5398028fe29f4521659cb425bb97aaa515efc297
srosenbauer <seth@bluenova.io>
2021-07-10 10:47:10 -0500

    node modules gone

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/Users/srosenbauer/Documents/GitHub/documentation/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/srosenbauer/Documents/GitHub/documentation/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/srosenbauer/Documents/GitHub/documentation/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/srosenbauer/Documents/GitHub/documentation/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/srosenbauer/Documents/GitHub/documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/srosenbauer/Documents/GitHub/documentation/src/css/custom.css"),
+];
---
+++ .docusaurus/routes.js
@@ -0,0 +1,131 @@
+
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+export default [
+{
+  path: '/',
+  component: ComponentCreator('/','deb'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug',
+  component: ComponentCreator('/__docusaurus/debug','3d6'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/config',
+  component: ComponentCreator('/__docusaurus/debug/config','914'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/content',
+  component: ComponentCreator('/__docusaurus/debug/content','c28'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/globalData',
+  component: ComponentCreator('/__docusaurus/debug/globalData','3cf'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/metadata',
+  component: ComponentCreator('/__docusaurus/debug/metadata','31b'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/registry',
+  component: ComponentCreator('/__docusaurus/debug/registry','0da'),
+  exact: true,
+},
+{
+  path: '/__docusaurus/debug/routes',
+  component: ComponentCreator('/__docusaurus/debug/routes','244'),
+  exact: true,
+},
+{
+  path: '/blog',
+  component: ComponentCreator('/blog','569'),
+  exact: true,
+},
+{
+  path: '/blog/hello-world',
+  component: ComponentCreator('/blog/hello-world','07a'),
+  exact: true,
+},
+{
+  path: '/blog/hola',
+  component: ComponentCreator('/blog/hola','6e6'),
+  exact: true,
+},
+{
+  path: '/blog/tags',
+  component: ComponentCreator('/blog/tags','e13'),
+  exact: true,
+},
+{
+  path: '/blog/tags/docusaurus',
+  component: ComponentCreator('/blog/tags/docusaurus','738'),
+  exact: true,
+},
+{
+  path: '/blog/tags/facebook',
+  component: ComponentCreator('/blog/tags/facebook','2fe'),
+  exact: true,
+},
+{
+  path: '/blog/tags/hello',
+  component: ComponentCreator('/blog/tags/hello','263'),
+  exact: true,
+},
+{
+  path: '/blog/tags/hola',
+  component: ComponentCreator('/blog/tags/hola','8b3'),
+  exact: true,
+},
+{
+  path: '/blog/welcome',
+  component: ComponentCreator('/blog/welcome','015'),
+  exact: true,
+},
+{
+  path: '/markdown-page',
+  component: ComponentCreator('/markdown-page','be1'),
+  exact: true,
+},
+{
+  path: '/docs',
+  component: ComponentCreator('/docs','533'),
+  
+  routes: [
+{
+  path: '/docs/getting-started/autogenerate-tests',
+  component: ComponentCreator('/docs/getting-started/autogenerate-tests','2ae'),
+  exact: true,
+},
+{
+  path: '/docs/getting-started/installing-otto',
+  component: ComponentCreator('/docs/getting-started/installing-otto','5f3'),
+  exact: true,
+},
+{
+  path: '/docs/getting-started/markdown-features',
+  component: ComponentCreator('/docs/getting-started/markdown-features','e4d'),
+  exact: true,
+},
+{
+  path: '/docs/getting-started/preview-environment',
+  component: ComponentCreator('/docs/getting-started/preview-environment','a11'),
+  exact: true,
+},
+{
+  path: '/docs/intro',
+  component: ComponentCreator('/docs/intro','e84'),
+  exact: true,
+},
+]
+},
+{
+  path: '*',
+  component: ComponentCreator('*')
+}
+];
